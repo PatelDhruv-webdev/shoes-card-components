611cc3257af7cf94ba0ed59684dbb90b07c95a34
Patel Dhruv <dhruv3415@gmail.com>
2024-02-08 22:56:08 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div> 
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn w-23 bg-blue-600 h-24 hover:{ font-bold }">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </div>
+      
+  )
+}
+
+export default App
